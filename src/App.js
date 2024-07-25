44ca2d75dc014344160c7b547449704490c081f6
bommisetty swathi <bommisettyswathi@gmail.com>
2024-07-25 22:44:43 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+// App.js
+import React from "react";
+import "./App.css";
+class App extends React.Component {
+	// Constructor
+	constructor(props) {
+		super(props);
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+		this.state = {
+			items: [],
+			DataisLoaded: false,
+		};
+	}
+
+	// ComponentDidMount is used to
+	// execute the code
+	componentDidMount() {
+		fetch("https://jsonplaceholder.typicode.com/users")
+			.then((res) => res.json())
+			.then((json) => {
+				this.setState({
+					items: json,
+					DataisLoaded: true,
+				});
+			});
+	}
+	render() {
+		const { DataisLoaded, items } = this.state;
+		if (!DataisLoaded)
+			return (
+				<div>
+					<h1> Pleses wait some time.... </h1>
+				</div>
+			);
+
+		return (
+			<div className="App">
+				<h1 className="geeks">GeeksforGeeks</h1>
+				<h3>Fetch data from an api in react</h3>
+				<div className="container">
+					{items.map((item) => (
+						<div className="item">
+							<ol key={item.id}>
+								<div>
+									<strong>
+										{"User_Name: "}
+									</strong>
+									{item.username},
+								</div>
+								<div>
+									Full_Name: {item.name},
+								</div>
+								<div>
+									User_Email: {item.email}
+								</div>
+							</ol>
+						</div>
+					))}
+				</div>
+			</div>
+		);
+	}
 }
 
 export default App;
